431219dceb3fdd6a91f55821402d1b52cc96cc81
Roland Bernard <rolli.bernard@gmail.com>
2019-10-20 22:06:00 +0200

    Fixed /drive/:uuid/history for sharepoints

--- src/server/sql/getentryhistory.sql
+++ src/server/sql/getentryhistory.sql
@@ -3,4 +3,14 @@ SELECT  h.hdatetime AS datetime,
         h.hoperation AS operation
 	FROM history AS h
     	JOIN users AS u ON (u.uid = h.huserid)
-    WHERE h.hentryid = UUID_TO_BIN(:entryuuid);
+    WHERE h.hentryid = UUID_TO_BIN(:entryuuid)
+UNION
+SELECT  h.hdatetime AS datetime,
+        u.uusername AS user,
+        h.hoperation AS operation
+	FROM sharepoints sp
+        JOIN shares AS s ON (sp.spshareid = s.sid)
+        JOIN history AS h ON (s.sentryid = h.hentryid)
+    	JOIN users AS u ON (u.uid = h.huserid)
+    WHERE sp.spid = UUID_TO_BIN(:entryuuid)
+ORDER BY datetime DESC;
